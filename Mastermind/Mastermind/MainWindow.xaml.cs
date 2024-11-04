00ac3c69c64ec690df03c7120d64c218d03ae00d
louie <11400565@student.pxl.be>
2024-11-04 15:35:15 +0100

    MasterMind-02 Code

--- Mastermind/Mastermind/MainWindow.xaml.cs
+++ Mastermind/Mastermind/MainWindow.xaml.cs
@@ -16,14 +16,52 @@ namespace Mastermind
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private List<(string, SolidColorBrush)> _colorOptions = new List<(string, SolidColorBrush)>();
+        private List<(string, SolidColorBrush)> _generatedColors = new List<(string, SolidColorBrush)>();
+        private List<(string, SolidColorBrush)> _selectedColors = new List<(string, SolidColorBrush)>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            InitGame();
+        }
+
+        private void InitGame()
+        {
+            _colorOptions.Add(("red", new SolidColorBrush(Colors.Red)));
+            _colorOptions.Add(("orange", new SolidColorBrush(Colors.Orange)));
+            _colorOptions.Add(("yellow", new SolidColorBrush(Colors.Yellow)));
+            _colorOptions.Add(("white", new SolidColorBrush(Colors.White)));
+            _colorOptions.Add(("green", new SolidColorBrush(Colors.Green)));
+            _colorOptions.Add(("blue", new SolidColorBrush(Colors.Blue)));
         }
 
         private void validateButton_Click(object sender, RoutedEventArgs e)
         {
+            List<(string name, SolidColorBrush color)> selectedColors = GenerateRandomColorCodes();
+            if (selectedColors.Any() && selectedColors.Count == 4)
+            {
+                string selectedColorString = string.Join(',', selectedColors.Select(x => x.name));
+                mainWindow.Title = $"Mastermind ({selectedColorString})";
+            }
+        }
 
+        private List<(string name, SolidColorBrush color)> GenerateRandomColorCodes()
+        {
+            List<(string, SolidColorBrush)> selectedOptions = new List<(string, SolidColorBrush)>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                (string, SolidColorBrush) keyPair = _colorOptions.ElementAt(new Random().Next(0, _colorOptions.Count()));
+                if (keyPair != default)
+                {
+                    selectedOptions.Add(keyPair);
+                }
+            }
+
+            return selectedOptions;
         }
 
 
